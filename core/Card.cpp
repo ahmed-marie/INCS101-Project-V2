59ef2e13465b4e76da8852c45dc4801426cc2423
ahmed-marie <ahmedkamelmarie@gmail.com>
2026-07-19 08:53:04 +0300

    Card Class Implementation
    Implemented the Card.cpp file

---
+++ core/Card.cpp
@@ -0,0 +1,39 @@
+#include "Card.h"
+
+Card::Card(int number, bool faceUp)
+{
+	this->number = number;
+	this->faceUp = faceUp;
+}
+
+
+Card::~Card() {}
+
+
+void Card::setNumber(int number)
+{
+	this->number = number;
+}
+
+
+int Card::getNumber() const
+{
+	return number;
+}
+
+
+void Card::setFaceUp(bool faceUp)
+{
+	this->faceUp = faceUp;
+}
+
+
+bool Card::isFaceUp() const
+{
+	return faceUp;
+}
+
+CardType getCardType() const
+{
+	return cardType;
+}
